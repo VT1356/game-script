94ccce28a3634f5fb566e7a20ca6e7ac21f4abe8
Nguyễn Hoài Nam <namptit307@gmail.com>
2015-03-18 01:11:50 +0700

    Create NAMNH_BAI1_TUAN11.sh

---
+++ tuan11/NAMNH_BAI1_TUAN11.sh
@@ -0,0 +1,60 @@
+# Nguyen Hoai Nam
+echo "Scrip cai dat apache tren Ubuntu va CenOS ban 64 bit"
+echo "Author: Nguyen Hoai Nam - VDC"
+
+# Kiem tra thong tin may
+tt=`cat /etc/issue.net | awk 'FNR == 1 {print}' | awk '{print $1}'`
+echo Ban dang dung HDH:$tt
+# Thuc hien viec cai dat
+if [ "$tt" = "Ubuntu" ] ; then
+	check=`dpkg -l | grep -w apache2-bin | awk '{print $2}'`
+	if [ "$check" = "apache2-bin" ] ; then
+	echo "May ban da la web-server voi phien ban `dpkg -l | grep apache2-bin | awk '{print $3}'`"
+	echo "Truy cap theo dia chi:"
+	ifconfig | grep -w inet | grep Bcast | awk -F ':' '{print $2}' | awk '{print $1}'
+	echo "thong qua port:`cat /etc/apache2/ports.conf | grep Listen |  awk 'FNR == 1 {print}' | awk '{print $2}'`"
+	else
+	echo "Update he thong!!!"
+	sleep 2
+	apt-get update
+	apt-get install -y apache2
+	tput setaf 2
+	echo "Nhap port ban muon doi:"
+	read p
+	tput setaf 7
+	sed -i "s/80/$p/" /etc/apache2/ports.conf
+	service apache2 restart
+	# Xong
+	echo "Da cai xong web-server. Dia chi web-server la:`ifconfig | grep -w inet | grep Bcast | awk -F ':' '{print $2}' | awk '{print $1}'`"
+	echo "Voi port la port ban vua chuyen !!! "
+	fi
+elif [ "$tt" = "CentOS" ] ; then
+	check=`yum list | grep -w httpd.x86_64 | awk '{print $1}'`
+	if [ "$check" = "httpd.x86_64" ]; then
+	echo "May ban da la web-server voi phien ban:`yum list | grep -w httpd.x86_64 | awk '{print $2}'`"
+	echo "Truy cap theo dia chi:"
+	ifconfig | grep -w inet | grep Bcast | awk -F ':' '{print $2}' | awk '{print $1}'
+	echo "thong qua port:`cat /etc/httpd/conf/httpd.conf | grep  Listen | grep -v ^# | awk '{print $2}'`"
+	else
+	yum -y install httpd
+	tput setaf 2
+	echo "Nhap port ban muon doi:"
+	read p
+	tput setaf 7
+	sed -i "s/Listen 80/Listen $p/" /etc/httpd/conf/httpd.conf
+	service httpd restart
+	echo "Da cai xong web-server. Dia chi web-server la:`ifconfig | grep -w inet | grep Bcast | awk -F ':' '{print $2}' | awk '{print $1}'`"
+	echo "Voi port la port ban vua chuyen !!! "
+	fi
+else
+echo "HDH may ban khong phai la Linux hoac CenOS. Bye"
+fi
+
+	
+	
+	
+
+
+
+
+
